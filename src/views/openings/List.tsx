82594e24b02f14a5d4159ab81a89f76ebb8e4a4c
Emre Guneyler <eguneys@gmail.com>
2025-03-04 17:16:20 +0300

    featured list help

--- src/views/openings/List.tsx
+++ src/views/openings/List.tsx
@@ -1,8 +1,10 @@
 import { A, useNavigate } from '@solidjs/router'
 import './List.scss'
 import { StudiesDBContext, StudiesDBProvider } from '../../components/sync_idb_study'
-import { createEffect, createResource, For, Show, Suspense, useContext } from 'solid-js'
+import { createResource, createSignal, For, Show, Suspense, useContext } from 'solid-js'
 import { Study } from '../../components/StudyComponent'
+import annotate_png from '../../assets/images/annotate.png'
+import compact_png from '../../assets/images/compact.png'
 
 export default () => {
     return (<>
@@ -31,46 +33,89 @@ const ListComponent = () => {
         navigate('/openings/' + study.id)
     }
 
+    const [tab, set_tab] = createSignal('mine')
+
     return (<>
     <main class="openings-list">
-        <div class='tools'>
-            <button onClick={() => on_new_opening()} class='new'><i data-icon=""></i> New Opening Repertoire</button>
-        </div>
-        <div class='tabs'>
-        </div>
-        <div class='list'>
-            <Suspense fallback={
-                <div class='loading'>Loading your openings..</div>}
-                >
-            <For each={studies()} fallback={
-                <div class='no-studies'>
-                    <h4>No Openings here yet.</h4>
-                    <p>Try importing a lichess study.</p>
-                    <p>Or build an opening repertoire using 
-                        <A href='/builder'>Builder</A> feature</p>
+        <aside class='subnav'>
+            <nav>
+                <a onClick={() => set_tab('mine')} class={'mine' + (tab() === 'mine' ? ' active': '')}>My Openings</a>
+                <a onClick={() => set_tab('featured')} class={'featured' + (tab() === 'featured' ? ' active': '')}>Featured</a>
+                <a onClick={() => set_tab('help')} class={'help' + (tab() === 'help' ? ' active': '')}><i data-icon=""></i>Help</a>
+            </nav>
+        </aside>
+        <div class='content'>
+            <Show when={tab()==='help'}>
+                <Help/>
+            </Show>
+            <Show when={tab()==='featured'}>
+                <h3 class='featured'> Featured Openings </h3>
+                <p class='featured'>If you want to feature your openings here, please <A href='/contact'>contact me</A>.</p>
+            </Show>
+            <Show when={tab()==='mine'}>
+                <div class='tools'>
+                    <button onClick={() => on_new_opening()} class='new'><i data-icon=""></i> New Opening Repertoire</button>
                 </div>
-            }>{study => 
-                <div class='study'>
-              <h3 onClick={() => on_click_study(study)} class='title'>{study.name}</h3>
-              <div class='sections'>
-                <Show when={study.sections.length === 1} fallback={
-                    <For each={study.sections}>{ section => 
-                        <div class='section'><i data-icon=""></i><span class='title'>{section.name}</span></div>
-                    }</For>
-                }>
-                    <div class='section'>
-                      <div class='chapters'>
-                        <For each={study.sections[0].chapters}>{ chapter => 
-                            <div class='chapter'><i data-icon=""></i>{chapter.name}</div>
-                        }</For>
+                <div class='list'>
+                    <Suspense fallback={
+                        <div class='loading'>Loading your openings..</div>}
+                        >
+                    <For each={studies()} fallback={
+                        <div class='no-studies'>
+                            <h4>No Openings here yet.</h4>
+                            <p>Try importing a lichess study.</p>
+                            <p>Or build an opening repertoire using 
+                                <A href='/builder'>Builder</A> feature</p>
+                        </div>
+                    }>{study => 
+                        <div class='study'>
+                      <h3 onClick={() => on_click_study(study)} class='title'><i data-icon=""></i>{study.name}</h3>
+                      <div class='sections'>
+                        <Show when={study.sections.length === 1} fallback={
+                            <For each={study.sections}>{ section => 
+                                <div class='section'><i data-icon=""></i><span class='title'>{section.name}</span></div>
+                            }</For>
+                        }>
+                            <div class='section'>
+                              <div class='chapters'>
+                                <For each={study.sections[0].chapters}>{ chapter => 
+                                    <div class='chapter'><i data-icon=""></i>{chapter.name}</div>
+                                }</For>
+                              </div>
+                            </div>
+                        </Show>
                       </div>
-                    </div>
-                </Show>
-              </div>
-              </div>
-            }</For>
-            </Suspense>
+                      </div>
+                    }</For>
+                    </Suspense>
+                </div>
+        </Show>
         </div>
     </main>
+    </>)
+}
+
+
+function Help() {
+    return (<>
+    <div class='help'>
+        <h2>Openings Feature</h2>
+        <p>
+            A learning tool to save your opening lines. Similar to, but a little more flexible than, lichess studies.
+        </p>
+        <h3> Annotate Positions </h3>
+        <p>
+            <img alt="annotate positions" src={annotate_png}></img>
+        </p>
+        <h3> Compact View of variations </h3>
+        <p>
+            <img alt="compact variations" src={compact_png}></img>
+        </p>
+        <h3> Local First </h3>
+        <p>
+            Everything is saved on your computer locally, no cloud involved. Also no collaboration features.
+        </p>
+
+    </div>
     </>)
 }
