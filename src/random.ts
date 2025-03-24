903f758444f0c95ea16405f99a9166cc67898350
Emre Guneyler <eguneys@gmail.com>
2025-03-24 20:20:30 +0300

    arr shuffle

--- src/random.ts
+++ src/random.ts
@@ -29,3 +29,21 @@ export function arr_rnd<A>(arr: Array<A>) {
 export function arr_remove<A>(arr: Array<A>, a: A) {
   arr.splice(arr.indexOf(a), 1)
 }
+
+
+export function arr_shuffle<A>(array: Array<A>) {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array
+}
